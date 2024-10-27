4f66e047f4407ac0fe69f3b0858e449027eb4d83
RoGalletti <160795760+RoGalletti@users.noreply.github.com>
2024-10-27 00:14:20 -0300

    probando por 2da vez .gitignore

---
+++ js/practica.js
@@ -0,0 +1,41 @@
+
+/*let nombreUsuario = prompt ("Ingrese nombre de usuario");
+
+if (nombreUsuario == "") {
+    alert("No ingresaste el nombre de usuario");
+}
+else {
+    
+    alert("Nombre de usuario ingresado: " + nombreUsuario);
+    
+}*/
+
+
+/*let nombre=prompt("Ingresá tu nombre");
+let apellido=prompt("Ingresá tu apellido");
+let edad=prompt("Ingresá tu edad");*/
+
+/*if ((nombre!="")&& (apellido!="")){
+    alert("Nombre: "+ nombre + "\nApellido: "+ apellido);
+}
+else {
+    alert("Error: Ingresar nombre y apellido");
+}
+if(edad<=18){
+    alert("Lo siento, acceso restringido");
+}
+else{
+    alert("Continuar");
+}*/
+
+/*let nombre=prompt("Ingrese su nombre");
+let apellido=prompt("Ingrese su apellido");
+let edad=prompt("Ingrese su edad");*/
+
+/////////////////////////////
+
+for( let i=0; i<10;i++){
+    alert(i);
+}
+
+
