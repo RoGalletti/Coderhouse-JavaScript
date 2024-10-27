56206fff529c54d39e47a3b304f4be1e1bccf5de
RoGalletti <160795760+RoGalletti@users.noreply.github.com>
2024-10-27 00:10:40 -0300

    cambios no guardados

--- js/app.js
+++ js/app.js
@@ -1,27 +1,38 @@
 let alumnos = [];
-let numAlumnos = 30;
 
-for (let i = 0; i < numAlumnos; i++) {
-    let nombre = prompt(`Ingrese el nombre del alumno ${i + 1}:`);
-    let nota1 = parseFloat(prompt(`Ingrese la primera nota de ${nombre}:`));
-    let nota2 = parseFloat(prompt(`Ingrese la segunda nota de ${nombre}:`));
-    let nota3 = parseFloat(prompt(`Ingrese la tercera nota de ${nombre}:`));
+while(true){
+    let nombreAlumno = prompt ("Ingrese el nombre del alumno");
 
-    // Validar que las notas estén en el rango correcto
+    alumnos.push ({
+        nombre: nombreAlumno,
+        notaFinal: 0
+    })
+
+    let opcion = prompt ("Desea agregar otro alumno? \n 1- Si \n 2- No");
+
+    if(opcion ==2 ){
+        break;
+    }
+};
+
+for (let i = 0; i < alumnos.length; i++) {
+    let alumno = alumnos[i]
+
+    let nota1 = parseFloat(prompt(`Ingrese la primera nota de ${alumno.nombre}:`));
+    let nota2 = parseFloat(prompt(`Ingrese la segunda nota de ${alumno.nombre}:`));
+    let nota3 = parseFloat(prompt(`Ingrese la tercera nota de ${alumno.nombre}:`));
+
+    
     if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10 || nota3 < 0 || nota3 > 10) {
-        console.log(`Las notas de ${nombre} deben estar entre 0 y 10.`);
-        i--; // Para no contar este intento
+        console.log("Las notas de ${alumnos.nombre} deben estar entre 0 y 10.");
+        i--;
         continue;
     }
 
     let notaFinal = (nota1 + nota2 + nota3) / 3;
-    alumnos.push({ nombre: nombre, notaFinal: notaFinal.toFixed(2) });
+    alumno.notaFinal = notaFinal;
+
+    alumnos[i]= alumnos       
+        
+        
 }
-
-console.log("Notas finales de los alumnos:");
-for (let i = 0; i < alumnos.length; i++) {
-    console.log(`${alumnos[i].nombre}: ${alumnos[i].notaFinal}`);
-}
-
-
-
